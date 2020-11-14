3d64f1e3ec2adb158827372aa63192e7c9785b00
gtc59891 <71907543+gtc59891@users.noreply.github.com>
2020-11-14 15:14:02 -0800

    Vuforia Scene Added
    Use the Vuforia Scene
    !!SWITCH OUT LICENCE KEY!!

---
+++ Space3D/Assets/Scripts/UI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class UI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject welcomePanel;
+
+    [SerializeField]
+    private Button dismissButton;
+
+    void Awake(){
+        dismissButton.onClick.AddListener(Dismiss);
+    }
+
+    private void Dismiss() => welcomePanel.SetActive(false);
+
+}
