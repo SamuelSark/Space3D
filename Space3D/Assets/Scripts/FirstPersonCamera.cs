39bd67e6fa79e5973eb6e7438acf49210131850b
Karven <karven.rodrigo.512@my.csun.edu>
2020-11-16 20:02:35 -0800

    Nov16 MVP
    Added basic 3D components for MVP.

---
+++ Space3D/Assets/Scripts/FirstPersonCamera.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstPersonCamera : MonoBehaviour
+{
+    public float moveSpeed;
+
+    public void Update(){
+        float v = Input.GetAxis ("Vertical") * moveSpeed;
+        transform.Translate  (new Vector3(0, 0, v) * Time.deltaTime);
+        /*GameObject sphere = (GameObject) Instantiate(spawnPrefab, transform.position, Quaternion.identity);
+        sphere.transform.LookAt(Camera.main.transform);*/
+
+    }
+}
+    
