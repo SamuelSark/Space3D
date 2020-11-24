0f974b15043f42a3a895d826df444fb3a04881f7
gtc59891 <71907543+gtc59891@users.noreply.github.com>
2020-11-23 19:11:09 -0800

    backup b4 update

---
+++ Space3D/Assets/Scripts/ChangeMaterialOnCollision.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Collider))]
+
+public class ChangeMaterialOnCollision : MonoBehaviour
+{
+    //[SerializeField] dont need for public vars
+    public Material correctMat;
+    public Material incorrectMat;
+    private MeshRenderer m_meshRenderer;
+
+    private void Start()
+    {
+        m_meshRenderer = GetComponent<MeshRenderer>();
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if(other.gameObject.name == "Electron")
+        {
+            m_meshRenderer.material = correctMat;
+        }
+        else
+        {
+            m_meshRenderer.material = incorrectMat;
+        }
+    }
+
+}
---
+++ Space3D/Assets/Scripts/changeMaterial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeMaterial : MonoBehaviour {
+ 
+     public Color redcolor;
+     public Color bluecolor;
+ 
+ 
+     void OnTriggerEnter(Collider other) {
+ 
+         if (other.gameObject.CompareTag ("sphere")) 
+         {
+             Debug.Log ("It's ALIVE and red");
+             transform.GetComponent<Renderer>().material.color= redcolor;
+         }
+ 
+         if (other.gameObject.CompareTag ("square")) 
+         {
+             Debug.Log ("It's ALIVE and blue");
+             transform.GetComponent<Renderer>().material.color= bluecolor;
+         }
+     }
+ }
